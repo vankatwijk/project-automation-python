dab55af7b5c8e4bde10eb42010cde07d92f76131
vankatwijk <hendrikus.hpvk@gmail.com>
2020-03-25 13:10:00 +0100

    first version

---
+++ .my_commans.sh
@@ -0,0 +1,13 @@
+function create() {
+    cd
+    source .env
+    python create.py $1
+    cd $FILEPATH$1
+    git init
+    git remote add origin git@github.com:$USERNAME/$1.git
+    touch README.md
+    git add .
+    git commit -m "Initial commit"
+    git push -u origin master
+    code .
+}
---
+++ create.py
@@ -0,0 +1,21 @@
+
+import sys
+import os
+from github import Github
+from dotenv import load_dotenv
+
+load_dotenv()
+
+path = os.getenv("FILEPATH")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+
+def create():
+    folderName = str(sys.argv[1])
+    os.makedirs(path + str(folderName))
+    user = Github(username, password).get_user()
+    repo = user.create_repo(folderName)
+    print("Succesfully created repository {}".format(folderName))
+
+if __name__ == "__main__":
+    create()
